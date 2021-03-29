8b769724feea25a22472b67bafc4202deca765f9
= <hugo.gutierrezm28@gmail.com>
2021-03-28 19:02:20 -0500

    Creando componente layout

---
+++ components/layout/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function layout({ children }) {
+    return <>
+        <Head>
+            <title>Awesome Store</title>
+            <link rel='icon' href='/favicon.ico' />
+            <meta charSet='utf-8' />
+            <meta httpEquiv='X-UA-Compatible' content='IE-edge' />
+            <meta name='description' content='Awesome Store' />
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+        </Head>
+    </>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import Head from 'next/head'
-
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <title>Awesome store</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <p>Create project.</p>
-            </main>
-        </div>
-    )
+    return <div>Hola</div>
 }
